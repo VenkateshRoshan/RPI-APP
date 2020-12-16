1f72a4c530feafe2faa4b861c34e8b7f958e4a96
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2020-12-16 19:33:15 +0530

    room_4_updation

--- app/src/main/java/com/example/rpi/Room_4/Room_4.java
+++ app/src/main/java/com/example/rpi/Room_4/Room_4.java
@@ -11,22 +11,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.rpi.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Room_4 extends Fragment {
 
     public Button mButton1 , mButton2 , mButton3 , mButton4 ;
+    FirebaseDatabase mDatabase ;
+    DatabaseReference mRef ;
+    String mLightPath = "Room_4/Light" ;
+    String mFanPath = "Room_4/Fan" ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.room_4, container, false);
+        mDatabase = FirebaseDatabase.getInstance() ;
         mButton1 = root.findViewById(R.id.ON_LIGHT) ;
         mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Light ON , invisible , set B2 to visible
                 mButton2.setVisibility(view.VISIBLE);
-                mButton1.setVisibility(view.INVISIBLE);
+                mButton1.setVisibility(view.INVISIBLE) ;
+                mRef = mDatabase.getReference(mLightPath) ;
+                mRef.setValue("ON") ;
             }
         });
         mButton2 = root.findViewById(R.id.OFF_LIGHT) ;
@@ -36,6 +45,8 @@ public class Room_4 extends Fragment {
                 // Light OFF , invisible , set B1 to visible
                 mButton1.setVisibility(view.VISIBLE);
                 mButton2.setVisibility(view.INVISIBLE);
+                mRef = mDatabase.getReference(mLightPath) ;
+                mRef.setValue("OFF") ;
             }
         });
         mButton3 = root.findViewById(R.id.ON_FAN) ;
@@ -45,6 +56,8 @@ public class Room_4 extends Fragment {
                 // Fan ON , invisible , set B4 to visible
                 mButton4.setVisibility(view.VISIBLE);
                 mButton3.setVisibility(view.INVISIBLE);
+                mRef = mDatabase.getReference(mFanPath) ;
+                mRef.setValue("ON") ;
             }
         });
         mButton4 = root.findViewById(R.id.OFF_FAN) ;
@@ -54,6 +67,8 @@ public class Room_4 extends Fragment {
                 // Light OFF , invisible , set B3 to visible
                 mButton4.setVisibility(view.INVISIBLE);
                 mButton3.setVisibility(view.VISIBLE);
+                mRef = mDatabase.getReference(mFanPath) ;
+                mRef.setValue("OFF") ;
             }
         });
         return root;
