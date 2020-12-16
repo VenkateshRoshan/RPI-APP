94d1441c350e541e2e02067b427029cc9252c740
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2020-12-16 18:44:21 +0530

    Room 4 Button visibilty action

--- app/src/main/java/com/example/rpi/Room_4/Room_4.java
+++ app/src/main/java/com/example/rpi/Room_4/Room_4.java
@@ -21,9 +21,41 @@ public class Room_4 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.room_4, container, false);
         mButton1 = root.findViewById(R.id.ON_LIGHT) ;
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Light ON , invisible , set B2 to visible
+                mButton2.setVisibility(view.VISIBLE);
+                mButton1.setVisibility(view.INVISIBLE);
+            }
+        });
         mButton2 = root.findViewById(R.id.OFF_LIGHT) ;
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Light OFF , invisible , set B1 to visible
+                mButton1.setVisibility(view.VISIBLE);
+                mButton2.setVisibility(view.INVISIBLE);
+            }
+        });
         mButton3 = root.findViewById(R.id.ON_FAN) ;
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Fan ON , invisible , set B4 to visible
+                mButton4.setVisibility(view.VISIBLE);
+                mButton3.setVisibility(view.INVISIBLE);
+            }
+        });
         mButton4 = root.findViewById(R.id.OFF_FAN) ;
+        mButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Light OFF , invisible , set B3 to visible
+                mButton4.setVisibility(view.INVISIBLE);
+                mButton3.setVisibility(view.VISIBLE);
+            }
+        });
         return root;
     }
 }
