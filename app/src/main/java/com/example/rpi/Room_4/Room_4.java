d458a21c3cd871be79846f8cb698dbe196366b97
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2020-12-16 17:57:07 +0530

    Room_4.java variables initialzation

--- app/src/main/java/com/example/rpi/Room_4/Room_4.java
+++ app/src/main/java/com/example/rpi/Room_4/Room_4.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +14,16 @@ import com.example.rpi.R;
 
 public class Room_4 extends Fragment {
 
-
+    public Button mButton1 , mButton2 , mButton3 , mButton4 ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.room_4, container, false);
+        mButton1 = root.findViewById(R.id.ON_LIGHT) ;
+        mButton2 = root.findViewById(R.id.OFF_LIGHT) ;
+        mButton3 = root.findViewById(R.id.ON_FAN) ;
+        mButton4 = root.findViewById(R.id.OFF_FAN) ;
         return root;
     }
 }
