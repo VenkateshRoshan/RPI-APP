59378bf5b6626e3f4f6c1de03890dabc4036f50e
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2021-07-22 11:56:47 +0530

    Updated

--- Room_4.py
+++ Room_4.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+from firebase import firebase as fb
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
-PIN = 2
+PIN = 5
 GPIO.setup(PIN,GPIO.OUT)
 
 """
@@ -21,16 +22,33 @@ try:
         time.sleep(TIME_OUT)
 
 finally:
-    GPIO.cleanup()"""
+    GPIO.cleanup()"""        
+        
+class Room_4 :
+    def __init__ (self) :
+        self.LINK = 'https://iot-home-cdf8e-default-rtdb.firebaseio.com/'
+        self.mLightPath = 'Room_4/Light'
+        self.mFanPath = 'Room_4/Fan'
+        self.mFB = fb.FirebaseApplication(self.LINK,None)
+        #self.PIN_Light = 2
+        #self.PIN_Fan = 3
+        #GPIO.setup(self.PIN_Light , GPIO.OUT)
+        
+    def getStatus(self) :
+        self.LightStatus = self.mFB.get(self.mLightPath,None)
+        self.FanStatus = self.mFB.get(self.mFanPath,None)
+        return
     
-while True :
-    mode = input('Enter mode : ').lower()
-    if 'off' in mode :
-        GPIO.output(PIN,GPIO.HIGH)
-        print('OFF')
-    elif 'on' in mode :
-        GPIO.output(PIN,GPIO.LOW)
-        print('ON')
-    else :
-        exit()
+    def setStatus(self,val) :
+        pass
+    
+room = Room_4()
 
+while True :
+    room.getStatus()
+    if room.LightStatus == 'ON' :
+        GPIO.output(PIN,GPIO.LOW)
+    else :
+        GPIO.output(PIN,GPIO.HIGH)
+        
+GPIO.cleanup()
